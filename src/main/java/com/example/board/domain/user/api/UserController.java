8511006ef56b7553ed7577951eedc4deb30e5669
김기서 <5rne@naver.com>
2023-06-16 23:45:06 +0900

    feat: 회원가입 로직 구현

---
+++ src/main/java/com/example/board/domain/user/api/UserController.java
@@ -0,0 +1,27 @@
+package com.example.board.domain.user.api;
+
+import static org.springframework.http.MediaType.*;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.board.domain.user.dto.UserCreateRequest;
+import com.example.board.domain.user.service.UserService;
+import com.example.board.global.dto.IdResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+	private final UserService userService;
+
+	@PostMapping(produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+	public IdResponse join(@RequestBody UserCreateRequest userCreateRequest) {
+		return userService.join(userCreateRequest.username(), userCreateRequest.password());
+	}
+}
