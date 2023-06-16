3791246593c37685f0476f624cdc0442b25d3604
김기서 <5rne@naver.com>
2023-06-17 02:27:50 +0900

    feat: UserService 추가

---
+++ src/main/java/com/example/board/domain/user/service/UserService.java
@@ -0,0 +1,32 @@
+package com.example.board.domain.user.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.board.domain.user.model.User;
+import com.example.board.domain.user.exception.DuplicateUsernameException;
+import com.example.board.domain.user.repository.UserRepository;
+import com.example.board.global.dto.IdResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserService {
+
+	private final UserRepository userRepository;
+
+	@Transactional
+	public IdResponse join(String username, String password) {
+		userRepository.findByUsername(username)
+			.ifPresent(user -> {
+				throw new DuplicateUsernameException("이미 사용중인 ID입니다: " + user.getUsername());
+			});
+
+		User user = User.of(username, password);
+		userRepository.save(user);
+
+		return new IdResponse(user.getId());
+	}
+}
